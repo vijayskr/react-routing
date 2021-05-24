f2988f8f87988e0f3469f542f668c9c041253535
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2021-05-24 09:27:06 -0400

    Updated Router Learning
    Nested Routes, Redirect routes, exact routes etc.,

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Switch>
+        <Route path='/' exact>
+          <Redirect to='/welcome' />
+        </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/pages/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const Welcome = () => {
+    return (
+        <section>
+            <h1>Welcome Page</h1>
+            <Route path='/welcome/new-user'>
+                <p>Welcome New User</p>
+            </Route>
+        </section>
+    );
+}
+
+export default Welcome;
